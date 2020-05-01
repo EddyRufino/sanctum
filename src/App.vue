494f8e09997b0a32519b245a7b2e403f67585556
Eddy Rufino <eddyjaair@gmail.com>
2020-05-01 11:45:28 -0500

    funciona el login y logout

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,31 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+
+      <template v-if="$store.state.auth">
+        <span>{{ $store.state.user.name }}</span> |
+        <button @click="logout">Logout</button>
+      </template>
+
+      <template v-else>
+        <router-link to="/login">Login</router-link>
+      </template>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch("logout");
+      return this.$router.push("/login");
+    }
+  },
+}
+</script>
 
 <style>
 #app {
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <input v-model="form.email" type="email" placeholder="Email..."/> <br>
+      <input v-model="form.password" type="password" placeholder="password..." /> <br>
+      
+      <button>Login</button>
+
+    </form>
+  </div>
+</template>
+
+<script>
+
+  // import axios from 'axios';
+  // axios.defaults.withCredentials = true;
+  // axios.defaults.baseURL = 'http://localhost:8000';
+
+  export default {
+    data() {
+      return {
+        form: {
+          email: 'eddyjaair@gmail.com',
+          password: 'password'
+        }
+      }
+    },
+
+    methods: {
+      async login() {
+        await this.$store.dispatch("login", this.form);
+        return this.$router.push("/");
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
